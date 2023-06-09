c8c9185e26bbcff7a5e27a4453d507a32e9072af
programadorisgod <catapiascastro@gmail.com>
2023-06-09 17:11:51 -0500

    prueba

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client"; // Importar createRoot desde aquí
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./Home/Home.jsx";
 import "./Home/Css/Styles.css";
 import { Access } from "./Access/Access.jsx";
@@ -10,25 +10,21 @@ import { Dashboard } from "./Dashboard/Dashboard.jsx";
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <PageError />,
-  },
-  {
-    path: "/login",
-    element: <Access />,
-  },
-  {
-    path: "/register",
-    element: <Access Form="Register" />,
-  },
-  {
-    path: "/app",
-    element: <Dashboard />,
-  },
-]);
 
-root.render(<RouterProvider router={router} />);
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback='Loanding...'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Access />} />
+          <Route path="/register" element={<Access Form="Register" />} />
+          <Route path="/app" element={<Dashboard />} />
+          <Route path="*" element={<PageError />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
 
+root.render(<App />);
